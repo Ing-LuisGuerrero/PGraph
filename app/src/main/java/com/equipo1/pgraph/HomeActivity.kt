09f4eac9a8537dbcd9d291d63525348b10e3ac7e
Luis Guerrero <manuel.guerreroalns@uanl.edu.mx>
2020-10-22 03:30:52 -0500

    Drawer menu, navigation, and fragments working.

--- app/src/main/java/com/equipo1/pgraph/HomeActivity.kt
+++ app/src/main/java/com/equipo1/pgraph/HomeActivity.kt
@@ -1,22 +1,77 @@
 package com.equipo1.pgraph
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        btnLogout.setOnClickListener {
-            val auth = FirebaseAuth.getInstance()
-            auth.signOut()
+        setSupportActionBar(toolbar)
 
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+        nav_view.bringToFront()
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        nav_view.setCheckedItem(R.id.nav_register)
+        nav_view.setNavigationItemSelectedListener(this)
+
+
+    }
+
+    override fun onBackPressed() {
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+
+        var fragment: Fragment = RegisterFragment()
+
+        when (item.itemId) {
+            R.id.nav_register -> {
+                fragment = RegisterFragment()
+
+            }
+            R.id.nav_chart -> {
+                fragment = ChartFragment()
+            }
+            R.id.nav_logout -> {
+                val auth = FirebaseAuth.getInstance()
+                auth.signOut()
+
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+        }
+        toolbar.title = item.title
+        transaction.replace(R.id.fragmentContent, fragment)
+        transaction.commit()
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+
+        return true
     }
 }
---
+++ app/src/main/java/com/equipo1/pgraph/RegisterFragment.kt
@@ -0,0 +1,19 @@
+package com.equipo1.pgraph
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        container?.removeAllViews()
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+}
