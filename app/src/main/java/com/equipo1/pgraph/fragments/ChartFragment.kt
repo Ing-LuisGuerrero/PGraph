9f74ed2bcbdcd1378ff01d5aa19150d0734e7971
Luis Guerrero <manuel.guerreroalns@uanl.edu.mx>
2020-10-23 21:47:55 -0500

    ChartFragment working.

--- app/src/main/java/com/equipo1/pgraph/fragments/ChartFragment.kt
+++ app/src/main/java/com/equipo1/pgraph/fragments/ChartFragment.kt
@@ -1,11 +1,19 @@
 package com.equipo1.pgraph.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.equipo1.pgraph.R
+import com.equipo1.pgraph.providers.AuthProvider
+import com.equipo1.pgraph.providers.RegistersProvider
+import com.github.aachartmodel.aainfographics.aachartcreator.*
+import kotlinx.android.synthetic.main.fragment_chart.*
+import java.text.SimpleDateFormat
 
 class ChartFragment : Fragment() {
 
@@ -16,6 +24,89 @@ class ChartFragment : Fragment() {
         container?.removeAllViews()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chart, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        getData()
+
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+    @SuppressLint("SimpleDateFormat")
+    private fun getData() {
+        val authProvider = AuthProvider()
+        val registersProvider = RegistersProvider()
+        val id = authProvider.getUid()
+        if (id != null) {
+            rlLoadingChart.visibility = View.VISIBLE
+            registersProvider.getRegistersByUser(id).orderBy("datetime").get().addOnSuccessListener {documents ->
+                var systolic: Array<Any> = arrayOf()
+                var diastolic: Array<Any> = arrayOf()
+                var heartRate: Array<Any> = arrayOf()
+                var oxygenSaturation: Array<Any> = arrayOf()
+                var datetime: Array<Any> = arrayOf()
+
+                val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm a")
+
+                for (document in documents) {
+                    systolic += document.getLong("pressureSystolic")!!.toInt()
+                    diastolic += document.getLong("pressureDiastolic")!!.toInt()
+                    heartRate += document.getLong("hearthRate")!!.toInt()
+                    oxygenSaturation += document.getLong("oxygenSaturation")!!.toInt()
+                    val date = document.getDate("datetime")
+                    if(date != null) {
+                        datetime += simpleDateFormat.format(date)
+                    }
+                }
+
+                Toast.makeText(this.activity, datetime[0].toString(), Toast.LENGTH_LONG).show()
+
+
+                plotData(systolic, diastolic, heartRate, oxygenSaturation, datetime)
+
+                rlLoadingChart.visibility = View.INVISIBLE
+
+            }
+                .addOnFailureListener {
+                    rlLoadingChart.visibility = View.INVISIBLE
+                    Log.d("ERROR", it.message.toString())
+                }
+        }
+
+
+    }
+
+    //TODO: Plot datetime in xAxis
+    private fun plotData(systolic: Array<Any>, diastolic: Array<Any>, heartRate: Array<Any>, oxygenSaturation: Array<Any>, datetime: Array<Any>) {
+        val aaChartView = this.activity?.findViewById<AAChartView>(R.id.aa_chart_view)
+        val aaChartModel : AAChartModel = AAChartModel()
+            .chartType(AAChartType.Spline)
+            .backgroundColor("#fff")
+            .dataLabelsEnabled(false)
+            .yAxisTitle("")
+            .zoomType(AAChartZoomType.XY)
+            .gradientColorEnable(true)
+            .xAxisTickInterval(1)
+            .yAxisAllowDecimals(false)
+
+            .series(arrayOf(
+                AASeriesElement()
+                    .name("Sistolica")
+                    .data(systolic),
+                AASeriesElement()
+                    .name("Diastolica")
+                    .data(diastolic),
+                AASeriesElement()
+                    .name("Pulso cardiaco")
+                    .data(heartRate),
+                AASeriesElement()
+                    .name("Oxigenacion")
+                    .data(oxygenSaturation)
+            ))
+
+        aaChartView?.aa_drawChartWithChartModel(aaChartModel)
     }
 
 }
--- app/src/main/java/com/equipo1/pgraph/providers/RegistersProvider.kt
+++ app/src/main/java/com/equipo1/pgraph/providers/RegistersProvider.kt
@@ -4,12 +4,17 @@ import com.equipo1.pgraph.models.Register
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 
 class RegistersProvider {
     private val collection = FirebaseFirestore.getInstance().collection("Registers")
 
     fun insert(register: Register): Task<DocumentReference> {
         return collection.add(register)
+    }
+
+    fun getRegistersByUser(id: String): Query {
+        return collection.whereEqualTo("uid", id)
     }
 
 }
